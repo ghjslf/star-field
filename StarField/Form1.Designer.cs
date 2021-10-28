cef8ddf77a3b4a432d815fa7063a7b4fc45b1074
Pashkov <pashkov@example.com>
2021-10-28 15:11:08 +0300

    ititial commit

---
+++ StarField/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace StarField
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PictureBox = new System.Windows.Forms.PictureBox();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PictureBox
+            // 
+            this.PictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PictureBox.Location = new System.Drawing.Point(0, 0);
+            this.PictureBox.Name = "PictureBox";
+            this.PictureBox.Size = new System.Drawing.Size(800, 450);
+            this.PictureBox.TabIndex = 0;
+            this.PictureBox.TabStop = false;
+            // 
+            // Timer
+            // 
+            this.Timer.Interval = 1;
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form";
+            this.Text = "Star Field";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PictureBox;
+        private System.Windows.Forms.Timer Timer;
+    }
+}
+
---
+++ StarField/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace StarField
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        class Star
+        {
+            public float X { get; set; }
+            public float Y { get; set; }
+            public float Z { get; set; }
+
+        }
+
+        private Star[] stars = new Star[5000];
+
+        private Random random = new Random();
+
+        private Graphics graphics;
+
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            PictureBox.Image = new Bitmap(PictureBox.Width, PictureBox.Height);
+
+            graphics = Graphics.FromImage(PictureBox.Image);
+
+            for (int i = 0; i < stars.Length; i++)
+            {
+                stars[i] = new Star() 
+                {
+                    X = random.Next (-PictureBox.Width, PictureBox.Width),
+                    Y = random.Next(-PictureBox.Height, PictureBox.Height),
+                    Z = random.Next(1, PictureBox.Width)
+                };
+            }
+
+            Timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            graphics.Clear(Color.Black);
+
+            foreach(var star in stars)
+            {
+                DrawStar(star);
+                MoveStar(star);
+            }
+
+            PictureBox.Refresh();
+        }
+
+        private void MoveStar(Star star)
+        {
+            star.Z -= 10;
+
+            if (star.Z < 1)
+            {
+                star.X = random.Next(-PictureBox.Width, PictureBox.Width);
+                star.Y = random.Next(-PictureBox.Height, PictureBox.Height);
+                star.Z = random.Next(1, PictureBox.Width); // main
+            }
+        }
+
+        private void DrawStar(Star star)
+        {
+            float size = Scale(star.Z, 0, PictureBox.Width, 6, 0);
+
+            float x = Scale(star.X / star.Z, 0, 1, 0, PictureBox.Width) + PictureBox.Width / 2;
+
+            float y = Scale(star.Y / star.Z, 0, 1, 0, PictureBox.Height) + PictureBox.Height / 2;
+
+            graphics.FillEllipse(Brushes.White, x, y, size, size);
+        }
+
+        private float Scale(float n, float start1, float stop1, float start2, float stop2)
+        {
+            return ((n - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
+        }
+
+        private void Form_KeyDown(object sender, KeyEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
